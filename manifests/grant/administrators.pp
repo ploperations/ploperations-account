1d55142529f92b58520cd67b7e730410814373e1
Daniel Parks <daniel.parks+06301925@puppet.com>
2018-11-02 14:16:58 -0700

    Update account::grant defined types
    This improves documentation, and makes the Windows specific grants noops
    on *nix.

--- manifests/grant/administrators.pp
+++ manifests/grant/administrators.pp
@@ -1,14 +1,18 @@
-# Grant accounts Administrators access on Windows
+# @summary Grant accounts Administrators access on Windows
 #
 # This takes all accounts in a specified group, and adds them to the
-# Administrators group. For example, I might want to give all the sysadmins
-# administrative access:
+# Administrators group. On splatnix this does nothing.
 #
-# ~~~ puppet
-# account::grant::administrators { 'sysadmin': }
-# ~~~
+# @example Give all the sysadmins administrative access
+#   account::grant::administrators { 'sysadmin': }
+#
+# @param to_group
+#   The group to give administrative access to. Generally, you should just use
+#   the title.
 define account::grant::administrators (
   String[1] $to_group = $title,
 ) {
-  account::grant::group { "Administrators>${to_group}": }
+  if $facts['os']['family'] == 'windows' {
+    account::grant::group { "Administrators>${to_group}": }
+  }
 }
